24442824c1c9440829e445d212ac874826a7da5f
Abhishek Dubey <abhishekbhardwaj510@gmail.com>
2019-03-14 16:37:33 +0530

    Update aws_instance.go

--- aws_instance.go
+++ aws_instance.go
@@ -4,9 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/awserr"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/ec2"
+        "github.com/aws/aws-sdk-go/aws/awserr"
+        "github.com/aws/aws-sdk-go/aws/session"
+        "github.com/aws/aws-sdk-go/service/ec2"
 )
 
 func main() {
